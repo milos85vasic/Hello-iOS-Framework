af6c8db28fa77d7dfb1f8891f48640b45a1796a8
Milos Vasic <milos85vasic@gmail.com>
2021-04-27 13:23:55 +0200

    Tryout app.

---
+++ Framework Tryout/Framework Tryout/Framework_TryoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Framework_TryoutApp.swift
+//  Framework Tryout
+//
+//  Created by Милош Васић on 4/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Framework_TryoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
