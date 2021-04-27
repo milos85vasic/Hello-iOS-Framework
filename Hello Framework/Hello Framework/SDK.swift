0623a0eae1b4d3c673502e0caa7300dc4983df19
Milos Vasic <milos85vasic@gmail.com>
2021-04-27 13:14:19 +0200

    First commit.

---
+++ Hello Framework/Hello Framework/SDK.swift
@@ -0,0 +1,16 @@
+//
+//  SDK.swift
+//  Hello Framework
+//
+//  Created by Милош Васић on 4/27/21.
+//
+
+import Foundation
+
+class SDK {
+    
+    func hello() {
+        
+        print("Hello!")
+    }
+}
